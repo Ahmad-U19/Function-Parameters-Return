3277b4e54af802ab556c5a26609d1e7d2fba0210
Ahmad-U19 <ahmedoppal2592@gmail.com>
2024-11-13 22:25:06 +0500

    Final Code

---
+++ script.js
@@ -0,0 +1,23 @@
+function me(){
+    document.write("Hey There");
+}
+
+me();
+me();
+me();me();me();me();me();me();me();me();
+
+function sum( x, y){
+    var y=x+y;
+    document.write("<br>"+"Sum Function : "+y);
+}
+
+sum(30, 20);
+
+function s(a, b, c, d){
+    var sum=a+b+c;
+    return sum;
+}
+
+var tt=s(196, 19, 154);
+
+document.write("<br>"+tt);
